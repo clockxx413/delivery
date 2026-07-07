0d328ec5250581eb048583df74048e770f6536f5
Yiyan-wuxiao <xinbeifu19870314@163.com>
2026-07-07 12:44:09 +0800

    feat: MySQL适配完成 + 接口调通

--- src/main/java/com/express/delivery/DeliveryApplication.java
+++ src/main/java/com/express/delivery/DeliveryApplication.java
@@ -1,9 +1,11 @@
 package com.express.delivery;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.express.delivery.mapper")
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
